34c8f13d7c322acf58b3f600ca79e016c2f11de7
popengaga <Popengagens01@gmail.com>
2024-02-10 16:51:18 +0300

    Готово

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+function removeCard(event) {
+  const card = event.target.closest('.card');
+    if (card) {
+      card.remove();
+      console.log('Карточка удалена');
+    }
+}
 
-// @todo: DOM узлы
+function addCard(cardData) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.content.cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  return cardElement
+}
 
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = addCard(cardData);
+  placesList.appendChild(cardElement);
+});
