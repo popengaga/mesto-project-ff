3823e75bfec0b4d76c5ffd83e3a2cc704ce1cb08
popengaga <Popengagens01@gmail.com>
2024-02-11 13:32:35 +0300

    Исправление_ошибок_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const cardTemplate = document.querySelector('#card-template');
+
 function removeCard(event) {
   const card = event.target.closest('.card');
     if (card) {
@@ -6,18 +8,18 @@ function removeCard(event) {
     }
 }
 
-function addCard(cardData) {
-  const cardTemplate = document.querySelector('#card-template');
+function createCard(cardData, removeHadler) {
   const cardElement = cardTemplate.content.cloneNode(true);
   cardElement.querySelector('.card__title').textContent = cardData.name;
   cardElement.querySelector('.card__image').src = cardData.link;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  cardElement.querySelector('.card__image').alt = cardData.alt;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeHadler);
   return cardElement
 }
 
 const placesList = document.querySelector('.places__list');
 
 initialCards.forEach((cardData) => {
-  const cardElement = addCard(cardData);
+  const cardElement = createCard(cardData, removeCard);
   placesList.appendChild(cardElement);
 });
